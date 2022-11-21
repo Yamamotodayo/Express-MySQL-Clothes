be44002b379b07f76de3e1346c6f8b6fd05e8e30
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-11-22 00:11:43 +0900

    アップロード画面で画像のプレビュー追加。Tailwind CSS インストール

--- routers/mypage.js
+++ routers/mypage.js
@@ -6,12 +6,26 @@ const mypageRouter = Express.Router();
 // マイページの表示
 mypageRouter.get("/", (req, res) => {
 
-  const sql = "SELECT * FROM tables";
+  const sql = "SELECT id, path, name FROM tables";
 
   cone.query(sql, (err, result) => {
+
+    result.forEach(i => {
+      console.log("これは画像のpath: "+ (i.path));
+      console.log("これは画像のpath: "+ JSON.parse(i.path));
+      result
+    });
+
+    for (let i = 1; i < 1; i++) {
+      console.log(result[i]);
+      
+    }
+    console.log(result);
     res.render("mypage.ejs", { tables: result });
     // console.log(result);
   });
 });
+
+
 
 export default mypageRouter;
